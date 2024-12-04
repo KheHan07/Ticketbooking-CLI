0ba673ea84c5e604487f845f04c8a5e21c940372
Khehan Lakkitha <lakkithapeiris@gmail.com>
2024-12-04 20:08:59 +0530

    Modified Customer class to update functionality

--- Ticketing System CLI/src/Customer.java
+++ Ticketing System CLI/src/Customer.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Customer {
+public class Customer implements Runnable {
+    private TicketPool ticketPool;
+    private int retrievalRate;
+    private volatile boolean running = true;
+
+    public Customer(TicketPool ticketPool, int retrievalRate) {
+        this.ticketPool = ticketPool;
+        this.retrievalRate = retrievalRate;
+    }
+
+    @Override
+    public void run() {
+        while (running) {
+            for (int i = 0; i < retrievalRate; i++) {
+                ticketPool.removeTicket();
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public void stop() {
+        running = false;
+    }
 }
